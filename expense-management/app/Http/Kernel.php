a1175a43fb899aba61a905435880c57f4b5636be
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2025-04-14 20:40:03 +0100

    Database setup, models and migrations

---
+++ expense-management/app/Http/Kernel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's route middleware.
+     *
+     * These middleware may be assigned to groups or used individually.
+     *
+     * @var array<string, class-string|string>
+     */
+    protected $routeMiddleware = [
+        // ... (existing middleware)
+        'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+}
